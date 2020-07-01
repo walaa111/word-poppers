1573e76b59a105c50b7e2fa7e6f9b01b42105270
walaa111 <walaagamil18@gmail.com>
2020-07-01 21:41:52 +0200

    edit

---
+++ word-proppers/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,40 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-connectivity-monitor.connectivity",
+    "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+    "pluginId": "cordova-connectivity-monitor",
+    "clobbers": [
+      "window.connectivity"
+    ]
+  },
+  {
+    "id": "cordova-admob.AdMobAds",
+    "file": "plugins/cordova-admob/www/admob.js",
+    "pluginId": "cordova-admob",
+    "clobbers": [
+      "window.admob",
+      "window.tappx"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-connectivity-monitor": "1.2.2",
+  "cordova-play-services-version-adapter": "1.0.2",
+  "cordova-admob": "5.1.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ word-proppers/www/js/index.js
@@ -0,0 +1,133 @@
+/*
+
+ * Licensed to the Apache Software Foundation (ASF) under one
+
+ * or more contributor license agreements.  See the NOTICE file
+
+ * distributed with this work for additional information
+
+ * regarding copyright ownership.  The ASF licenses this file
+
+ * to you under the Apache License, Version 2.0 (the
+
+ * "License"); you may not use this file except in compliance
+
+ * with the License.  You may obtain a copy of the License at
+
+ *
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ *
+
+ * Unless required by applicable law or agreed to in writing,
+
+ * software distributed under the License is distributed on an
+
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+
+ * KIND, either express or implied.  See the License for the
+
+ * specific language governing permissions and limitations
+
+ * under the License.
+
+ */
+
+var app = {
+
+    // Application Constructor
+
+    initialize: function () {
+
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+
+
+
+
+    },
+
+
+
+    // deviceready Event Handler
+
+    //
+
+    // Bind any cordova events here. Common events are:
+
+    // 'pause', 'resume', etc.
+
+    onDeviceReady: function () {
+
+        this.receivedEvent('deviceready');
+
+ var inAppBrowserbRef = cordova.InAppBrowser.open('https://www.snappygames.com/en/games/word-poppers/play', '_self', 'location=no,toolbar=no,zoom=no,clearcache=yes,hidespinner=no,beforeload=yes');
+
+/*
+inAppBrowserbRef.addEventListener('loadstart', function() {
+  alert("yes");
+});
+*/
+
+        //admob
+     // Set AdMobAds options:
+      admob.setOptions({
+        publisherId:           "ca-app-pub-7251676025279948/8770272502",  // Required
+        interstitialAdId:      "ca-app-pub-7251676025279948/4831027492",  // Optional
+        autoShowBanner:        true,                                      // Optional
+        autoShowRInterstitial: false,                                     // Optional
+        autoShowRewarded:      false,                                     // Optional
+        tappxIdiOS:            "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+        tappxIdAndroid:        "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+        tappxShare:            0.5                                        // Optional
+      });
+      
+      // Start showing banners (atomatic when autoShowBanner is set to true)
+      admob.createBannerView();
+      
+      // Request interstitial ad (will present automatically when autoShowInterstitial is set to true)
+      admob.requestInterstitialAd();
+ 
+      // Request rewarded ad (will present automatically when autoShowRewarded is set to true)
+      admob.requestRewardedAd();
+       //admob
+
+
+
+    },
+
+
+
+    // Update DOM on a Received Event
+
+    receivedEvent: function (id) {
+
+        var parentElement = document.getElementById(id);
+
+        var listeningElement = parentElement.querySelector('.listening');
+
+        var receivedElement = parentElement.querySelector('.received');
+
+
+
+        listeningElement.setAttribute('style', 'display:none;');
+
+        receivedElement.setAttribute('style', 'display:block;');
+
+
+
+        console.log('Received Event: ' + id);
+
+
+    }
+
+};
+
+
+                //admob
+
+
+           //admob
+
+app.initialize();
